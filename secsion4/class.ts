6edb6dd044c0809d74c9ded11045de43e17c3ee2
ryo sasaki <7890bask@gmail.com>
2024-08-18 21:59:15 +0900

    add readonly comment

--- secsion4/class.ts
+++ secsion4/class.ts
@@ -1,10 +1,15 @@
 class Person {
+  // readonly修飾子は変数やパラメータに設定できる。(内部でも変更できない)
+  // public readonly id: number = 32;
+  // id = 30 これはできない
+  // readonly修飾子はconstructor内では再代入が可能である
+
   // publicを明示的に宣言することもできる。
   // initの変数の書き方は2パターンある
   // 1. initNameのように変数を準備
   // 2. Public, Privateとつけて変数と型を指定する
   constructor(
-    public name: string,
+    public readonly name: string,
     private age: number
   ) {}
 
